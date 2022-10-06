aa14f0dc05798d8b8cc6f07b2c9588e6c90b05d0
nug <nug@senza.local>
2022-10-06 17:35:52 +0530

    nikxgram hosting

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -5,14 +5,21 @@ var {class1} = require('../controller/controller');
 
 var path = require("path");
 
+router.get("/page",(req,res)=>{
+
+    console.log(process.env.SECRET_KEY);
+    res.sendFile(path.join(__dirname, "../404page.html"));
+    
+})
+
 router.get("/signup",class1.a);
 router.post("/signup",class1.b);
 router.get("/otp",class1.c);
 router.post("/otp",class1.d);
-router.get("/otp",class1.e);
-router.get('/login', class1.f);
-router.post('/login', class1.g);
-router.get("/first",class1.h);
+// router.get("/otp",class1.e);
+// router.get('/login', class1.f);
+// router.post('/login', class1.g);
+// router.get("/first",class1.h);
 
 router.get("/*",(req,res)=>{
 

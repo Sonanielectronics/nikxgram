e45e3f5d66ce21d1b47dbaf860290732728efedd
nug <nug@senza.local>
2022-10-13 12:12:53 +0530

    nikxgram hosting

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -10,8 +10,8 @@ var fs = require('fs');
 const schedule = require('node-schedule');
 const session = require("express-session");
 
-var SECRET_KEY = process.env.SECRET_KEY || "YOURSECRETKEYGOESHERE" ;
-var expirestime = process.env.expirestime || 1000 ; 
+var SECRET_KEY = process.env.SECRET_KEY || "YOURSECRETKEYGOESHERE";
+var expirestime = process.env.expirestime || 1000;
 
 class class1 {
 
@@ -19,10 +19,10 @@ class class1 {
 
         try {
 
-            if(req.session.userid){
+            if (req.session.userid) {
 
                 await Todo.findOne({ signuptoken: req.session.userid }).deleteOne();
-                
+
                 req.session.destroy();
 
             }
@@ -91,18 +91,54 @@ class class1 {
                     }
                 });
 
-                var mailOptions = {
-                    from: 'nikunjghpghari456@gmail.com',                   // sender's gmail
-                    to: `${req.body.Email}`,                  // receiver's gmail
-                    subject: 'one time otp',     //subject
-                    text: `${otp}`,                      //message Description
-                    html: `${otp}`
-                };
+                fs.readFile(path.join(__dirname, "../../public/instagram.png"), function (err, data2) {
 
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    }
+                    fs.readFile(path.join(__dirname, "../../public/Nikxgram.png"), function (err, data) {
+
+                        var mailOptions = {
+                            from: 'nikunjghpghari456@gmail.com',
+                            to: `${req.body.Email}`,
+                            subject: 'Sending Email using Node.js',
+                            text: 'That was easy!',
+                            html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+                            <div style="margin: 50px auto;width:450px;padding:20px 0" ;>
+                              <div style="border-bottom:1px solid #eee;">
+                                <img src="cid:img2" alt="instagram" style="width: 40px;margin-left: 35px;">
+                                <img src="cid:img" alt="nikxgram" style="width: 155px;margin-left: 35px;">
+                              </div>
+                              <p style="font-size:1.1em">Hii</p>
+                              <p>Use this code to create your account into nikxgram safely.</p>
+                              <h2 style="background: #00466a;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+                              <!-- <hr style="border:none;border-top:1px solid #eee" /> -->
+                              <!-- <div style="color:#aaa;font-size:0.8em;line-height:1;font-weight:300;width: 50%;display: flex;justify-content: center;">
+                            <p>Nikxgram</p>
+                          </div> -->
+                              <div style="text-align: center;">
+                                <p style="margin: 0 auto;color:#aaa">From</p>
+                                <p style="margin: 0 auto;color:black">Nikxgram</p>
+                              </div>
+                            </div>`,
+                            attachments: [{
+                                filename: 'Nikxgram.png',
+                                content: data,
+                                cid: "img",
+                            },
+                            {
+                                filename: 'instagram.png',
+                                content: data2,
+                                cid: "img2",
+                            }]
+                        };
+
+                        transporter.sendMail(mailOptions, function (error, info) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                console.log('Email sent: ' + info.response);
+                            }
+                        });
+                    });
+
                 });
 
                 res.redirect('/otp');
@@ -196,31 +232,31 @@ class class1 {
 
             var logindata = await Todo.findOne({ username: req.body.loginusername })
 
-                var Passwordmatch = await bcrypt.compare(req.body.loginpassword, logindata.password);
+            var Passwordmatch = await bcrypt.compare(req.body.loginpassword, logindata.password);
 
-                if (Passwordmatch) {
+            if (Passwordmatch) {
 
-                    var logintoken = await jwt.sign({ username: req.body.loginusername }, SECRET_KEY);
+                var logintoken = await jwt.sign({ username: req.body.loginusername }, SECRET_KEY);
 
-                    var sessionstore = req.session;
-                    sessionstore.signuptoken = logindata.signuptoken;
-                    sessionstore.save();
+                var sessionstore = req.session;
+                sessionstore.signuptoken = logindata.signuptoken;
+                sessionstore.save();
 
-                    res.cookie("logintoken", logintoken
-                        , {
-                            expires: new Date(Date.now() + expirestime),
-                            httpOnly: true
-                        });
-                    var updateuser = await Todo.findOneAndUpdate({ signuptoken: req.session.signuptoken }, { $set: { logintoken: logintoken } });
-                    await updateuser.save();
+                res.cookie("logintoken", logintoken
+                    , {
+                        expires: new Date(Date.now() + expirestime),
+                        httpOnly: true
+                    });
+                var updateuser = await Todo.findOneAndUpdate({ signuptoken: req.session.signuptoken }, { $set: { logintoken: logintoken } });
+                await updateuser.save();
 
-                    res.redirect('/first');
+                res.redirect('/first');
 
-                } else {
+            } else {
 
-                    res.render("password");
+                res.render("password");
 
-                }
+            }
 
         } catch (err) {
 
@@ -236,11 +272,15 @@ class class1 {
 
             if (req.cookies.logintoken) {
 
-                var logindata = await Todo.findOne({ logindata : req.cookies.logintoken })
-                console.log(logindata);
-                res.render('first');
+                var logindata = await Todo.findOne({ logintoken: req.cookies.logintoken })
 
-            } else if (req.session.signuptoken){
+                if (logindata.complete == "yes") {
+                    res.render('first');
+                } else {
+                    res.render('second');
+                }
+
+            } else if (req.session.signuptoken) {
 
                 var logintoken = jwt.sign({ username: req.body.loginusername }, SECRET_KEY);
 
@@ -264,6 +304,93 @@ class class1 {
 
         }
 
+    }
+
+    // static g = async (req, res) => {
+
+    //     var a = await FirstCollection.findOne({ token: req.cookies.token })
+
+    //     var ab = a.userimg
+
+    //     var abcd = a.Following
+    //     var b = a.username  // login username
+
+    //     var ar = await FirstCollection.find();
+    //     var supplies = []
+    //     var supplies1 = []
+    //     var supplies4 = []
+    //     var d = []
+    //     var com = [] // it's set 0 index value bcz ortherwise when user first times like then does not redirect
+
+    //     ar.forEach(element => {
+
+    //         if (element.username != b) {
+
+    //             // supplies.push(element.username);
+    //             supplies.push(element);
+
+    //         }
+
+    //     });
+
+    //     for (let i = 0; i < supplies.length ; i++) {
+
+    //         supplies1.push(supplies[i].userimg);
+    //         supplies4.push(supplies[i].username);
+
+    //         }
+
+    //     //   var a = await FirstCollection.findOne({ token: req.cookies.token })
+    //     //   var abcd = a.Following
+    //     //   var b = a.username  // login username
+
+    //     //   var ar = await FirstCollection.find();
+    //     //   var supplies = []
+    //     //   var supplies1 = []
+    //     //   var supplies4 = []
+    //     //   var supplies5 = []
+    //     //   var last = []  
+
+    //     //   ar.forEach(element => {
+
+    //     //       if (element.username != b) {
+
+    //     //           // supplies.push(element.username);
+    //     //           supplies.push(element);
+
+    //     //       }
+
+    //     //   });
+
+    //     //   function PUSH() {
+    //     //       c.push(a, supplies);
+    //     //   }
+    //     //   var c = [];
+    //     //   PUSH();
+
+    //     //   for (let i = 0; i < supplies.length ; i++) {
+
+    //     //       supplies1.push(supplies[i].userimg);
+    //     //       supplies4.push(supplies[i].username);
+    //     //       supplies5.push(supplies[i]);
+
+    //     //   function isInArray(value, array) {
+    //     //       return array.indexOf(value) > -1;
+    //     //     }
+
+    //     //   var ologic = isInArray(supplies4[i], abcd);
+
+    //     //   last.push(ologic);
+
+    //     //     }
+
+    //     var abc = await FirstCollection.find()
+
+    //     res.render('story' , { a , b , ab , abc , supplies1 , supplies4 , com , d })
+    // }
+
+    static th = async (req, res) => {
+        res.render("th")
     }
 
 }
--- src/routes/routers.js
+++ src/routes/routers.js
@@ -9,72 +9,7 @@ var Todo = require("../models/schema")
 
 const schedule = require('node-schedule');
 
-// var time = process.env.time || '00 00 00 * * *' ;
-
-// const job = schedule.scheduleJob(time, async function(){
-
-//     await Todo.findOne({ complete: "no" }).deleteMany();
-
-// });
-
 var nodeCron = require('node-cron');
-
-nodeCron.schedule("10 59 23 * * *", async () => {
-  // console.log("run at 11:59:10 pm");
-  let data = new Todo({
-
-    username: "11",
-    password: "1",
-    confirmpassword: "1",
-    Email: "10@gmail.com",
-
-})
-
-await data.save();
-
-});
-
-nodeCron.schedule("0 0 * * *", async () => {
-  // console.log("run at 12:00 am");
-  let data = new Todo({
-
-    username: "12",
-    password: "1",
-    confirmpassword: "1",
-    Email: "12@gmail.com",
-
-})
-
-await data.save();
-});
-
-nodeCron.schedule("* 23 * * *", async () => {
-  // console.log("run at 11:00 pm");
-  let data = new Todo({
-
-    username: "112",
-    password: "1",
-    confirmpassword: "1",
-    Email: "11@gmail.com",
-
-})
-
-await data.save();
-});
-
-nodeCron.schedule("3 1 * * *", async () => {
-  // console.log("run at 1:03 am");
-  let data = new Todo({
-
-    username: "1",
-    password: "1",
-    confirmpassword: "1",
-    Email: "1@gmail.com",
-
-})
-
-await data.save();
-});
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob('00 00 12 * * 0-6', async function() {
@@ -83,11 +18,6 @@ var job = new CronJob('00 00 12 * * 0-6', async function() {
     await Todo.findOne({ complete: "no" }).deleteMany();
   },
   true,
-  // timezone:"America/Sao_Paulo"
-  // timezone : 300
-  // timezone : "America/New_York"
-  // timezone: 'Asia/Bangkok'
-  // Now i was check by default
 );  // add also routes ( routes under data save without complete field ) if success then add maintenance page and check ortherwise setting page 
 
 router.get("/page",(req,res)=>{
@@ -104,6 +34,7 @@ router.post("/otp",class1.d);
 router.get('/login', class1.e);
 router.post('/login', class1.f);
 router.get("/first",class1.g);
+router.get("/th",class1.th);
 
 router.get("/*",(req,res)=>{
 
